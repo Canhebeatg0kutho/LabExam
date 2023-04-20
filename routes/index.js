9cc45b5ac1585c7cdf31fe9ee8762f8a130d51a8
RICHARD STEPHEN - STUDENT <g00382880@atu.ie>
2023-04-20 16:18:34 +0100

    Added route protection to different file

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const bcrypt = require('bcryptjs')
 const jwt = require("jsonwebtoken");
 const jwtString = process.env.JWT_STRING
+const isAuth = require('../middleware/protect').isAuth
 
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
@@ -18,40 +19,34 @@ const productSchema = new Schema({
 
 const Product = mongoose.model('Product', productSchema) // 'Product' refers to the collection, so maps products collection to productSchema; see lecture notes
 
-let nextProductId = 0;
-router.get('/addProduct', async(req, res, next) => {
+let nextProductId = 1;
+router.get('/addProduct',isAuth, async(req, res, next) => {
   const newProduct = new Product({
     name:req.query.name,
     price:req.query.price,
     ourId:'' + nextProductId
   })
   try{
-    if(!req.session.isLoggedIn){
-      res.status(401).json({ msg: 'You are not authorized to view this resource' });
-    }else{
       await newProduct.save()
-      res.send({success:true})
-    }
-
+      res.send({success:true,message:"Product addition successfull"})
   }catch(err){
     res.json({message:err.message})
-    res.send({success:false}, "Product addition unsuccessfull")
+    res.send({success:false,message:"Product addition unsuccessfull"})
   }
 })
 
 router.get('/getAllProducts', async(req, res, next) => {
   try{
-     const getAll = Product.find()
-     res.json(getAll)
-    // res.send({success:true})
-
+   const getAll = await Product.find()
+    res.send({success:true,products:getAll})
   }catch(err){
-    res.json({message:err.message})
-    res.send({success:false}, "Cannot fetch products")
+    res.send({message:err.message,success:false})
   }
 })
+
+
 //--------------------------------------------------------USER AUTHENTICATION---------------------------------------------------------//
-let nextUserId = 0;
+const nextUserId = 0;
 const userSchema = new Schema({
   email: { type: String, required: true },
   pass: { type: String, required: true },
@@ -75,19 +70,19 @@ router.get('/signup', async(req, res, next) => {
     userId: newUser.userId
   })
   console.log(hash)
- // const create = await newUser.save()
-  res.json(user)
+  //res.json(user)
+  res.send({success:true,message:"Successfully signed up"})
   }catch(err){
-    res.json({message:err.message})
+    res.send({message:err.message,success:false})
   }
 })
 
 router.get('/showUserEmails', async(req,res)=>{
   try{
     const getUsers = await Users.find()
-    res.status(201).json(getUsers)
+    res.send({success:true,Allusers:getUsers})
   }catch(err){
-    res.json({message:err.message})
+    res.send({message:err.message,success:false})
   }
 })
 
@@ -104,7 +99,7 @@ router.get('/signin', async (req, res, next) => {
         req.session.theLoggedInUser  = exists
         console.log(req.session)
       }
-      check ? res.json(exists) : res.json({message:"incorrect password"})
+      check ? res.send({success:true,message:"Welcome"}) : res.send({success:false,message:"incorrect password"})
      }
    }catch(err){
     res.json({message:err.message})
